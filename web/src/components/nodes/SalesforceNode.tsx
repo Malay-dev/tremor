03499d38adb4c26f6924ebbacec6cfd7267751f7
Malay Kumar <malaykumar2003@gmail.com>
2026-08-23 13:12:26 +0530

    feat(web): Salesforce node with real logo SVG

---
+++ web/src/components/nodes/SalesforceNode.tsx
@@ -0,0 +1,11 @@
+"use client";
+import NodeShell from "./NodeShell";
+import SalesforceLogo from "../icons/SalesforceLogo";
+
+export default function SalesforceNode() {
+  return (
+    <NodeShell>
+        <SalesforceLogo size={200}></SalesforceLogo>
+    </NodeShell>
+  );
+}
